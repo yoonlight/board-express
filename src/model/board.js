8550ceb45a3a254de629d1307a5631d98f43694f
yoonlight <yoonlight12@gmail.com>
2021-03-09 09:52:34 +0900

    update board model

--- src/model/board.js
+++ src/model/board.js
@@ -7,6 +7,21 @@ const boardSchema = new Schema({
   body: String,
   date: { type: Date, default: Date.now },
   secret: { type: Boolean, default: false },
+  comments: [
+    {
+      id: Number,
+      usernmae: String,
+      body: String,
+      secret: { type: Boolean, default: false },
+      secretName: { type: Boolean, default: false },
+      date: { type: Date, default: Date.now },
+    },
+  ],
+  id: Number,
+  tags: [String],
+  category: String,
+  like: Number,
+  view: Number,
 })
 
 const board = mongoose.model('Board', boardSchema)
