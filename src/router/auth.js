0e1ca1d803dbe08b7fbe0b512a334d2eeb3a985c
kjw <yoonlight@gmail.com>
2021-03-15 00:43:35 +0900

    using jwt

--- src/router/auth.js
+++ src/router/auth.js
@@ -12,18 +12,6 @@ router.get('/user', async (req, res) => {
   res.send(result)
 })
 
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    if (req.isAuthenticated()) {
-      res.send('success')
-    } else {
-      res.status(404).json('failure')
-    }
-  }
-)
-
 router.post('/register', (req, res, next) => {
   const username = req.body.username
   user.register(
@@ -47,55 +35,28 @@ router.post(
   }),
   async (req, res, next) => {
     const option = { expiresIn: '5m' }
-    req.login(user, { session: false }, (error) => {
+    const secretOrKey = process.env.SERVER_SECRET_KEY
+    const username = req.body.username
+    req.login(user, { session: false }, async (error) => {
       if (error) next(error)
-      const token = jwt.sign(
-        { uid: user._id },
-        process.env.SERVER_SECRET_KEY,
-        option
-      )
-      console.log(user._id)
-
+      const info = await user.findOne().where('username').equals(username)
+      const token = jwt.sign({ uid: info._id }, secretOrKey, option)
       return res.json({ message: 'success', token })
     })
   }
 )
 
 router.get(
-  '/a',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.send()
-  }
-)
-
-router.post(
   '/profile',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    res.send(req.user.profile)
+    res.send(req.user)
   }
 )
 
 router.get('/logout', (req, res) => {
   req.logout()
   res.json({ message: 'logout' })
-})
-
-router.get('/me', function (req, res) {
-  var token = req.headers['x-access-token']
-  console.log(req.headers)
-  if (!token)
-    return res.status(401).send({ auth: false, message: 'No token provided.' })
-
-  jwt.verify(token, process.env.SERVER_SECRET_KEY, function (err, decoded) {
-    if (err)
-      return res
-        .status(500)
-        .send({ auth: false, message: 'Failed to authenticate token.' })
-
-    res.status(200).send(decoded)
-  })
 })
 
 export default router
